8fb2f190d3f80d4bccb31a0de44a41715caf885e
shreyasgrampurohit <shreyasg0512@gmail.com>
2023-05-03 17:35:40 +0530

    tested

--- pipe_reg.vhdl
+++ pipe_reg.vhdl
@@ -1,17 +1,22 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
 entity pipe_reg is
 	port (
             clock: in std_logic;
             PR_WR: in std_logic;
             Data_In: in std_logic_vector(95 downto 0);
-            Data_Out: in std_logic_vector(95 downto 0)
+            Data_Out: out std_logic_vector(95 downto 0)
         );
 end pipe_reg;
-    signal data: std_logic_vector(95 downto 0) := (others => '0');
+    
 architecture behavioural of pipe_reg is
+		signal data: std_logic_vector(95 downto 0) := (others => '0');
+begin
     Data_Out <= data;
     clock_proc: process(clock, data)
     begin
-        if(clock = '0' and clock' event) then
+        if(clock = '1' and clock' event) then
             if(PR_WR = '1') then
                 data <= Data_In;
             end if;
